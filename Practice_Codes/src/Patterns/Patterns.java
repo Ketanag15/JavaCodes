18482ca869266a77c3c55a6266918c4955453912
Ketanag15 <Ketanag2k@gmail.com>
2025-05-14 23:35:47 +0530

    patterns questions characters

--- Practice_Codes/src/Patterns/Patterns.java
+++ Practice_Codes/src/Patterns/Patterns.java
@@ -3,7 +3,7 @@ package Patterns;
 public class Patterns {
     public static void main(String[] args) {
         int n = 5;
-        reverseUpsideRightTriangle(n);
+        charRepeat(n);
     }
 
     /*
@@ -83,6 +83,67 @@ public class Patterns {
             for(int k=i; k>=1; k--){
                 System.out.print("*");
             }
+            System.out.println();
+        }
+    }
+    /*
+    1
+    22
+    333
+    4444
+    55555
+     */
+    public static void numbersI(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    /*
+    1
+    12
+    123
+    1234
+     */
+    public static void counts(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    /*
+    A
+    AB
+    ABC
+    ABCD
+     */
+    public static void characterPrint(int n){
+        for(int i=1; i<=n; i++){
+            char ch = 'A';
+            for(int j=1; j<=i; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    /*
+    A
+    BB
+    CCC
+    DDDD
+     */
+    public static void charRepeat(int n){
+        char ch = 'A';
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(ch);
+            }
+            ch++;
             System.out.println();
         }
     }
