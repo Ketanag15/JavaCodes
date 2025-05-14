db03490389c30181e4940797b615bb7b2961e581
Ketanag15 <Ketanag2k@gmail.com>
2025-05-14 23:23:00 +0530

    patterns questions

--- Practice_Codes/src/Patterns/Patterns.java
+++ Practice_Codes/src/Patterns/Patterns.java
@@ -3,10 +3,10 @@ package Patterns;
 public class Patterns {
     public static void main(String[] args) {
         int n = 5;
-        upsideRightTriangle(n);
+        reverseUpsideRightTriangle(n);
     }
 
-    /*v
+    /*
      ****
      ****
      ****
@@ -63,6 +63,24 @@ public class Patterns {
     public static void upsideRightTriangle(int n){
         for(int i=n; i>=1; i--){
             for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    /*
+    *****
+     ****
+      ***
+       **
+        *
+     */
+    public static void reverseUpsideRightTriangle(int n){
+        for(int i=n; i>=1; i--){
+            for(int j=0; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=i; k>=1; k--){
                 System.out.print("*");
             }
             System.out.println();
