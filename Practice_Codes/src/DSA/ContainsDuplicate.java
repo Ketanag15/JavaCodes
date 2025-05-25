514390133dfe1aea6d41b9b763056ead49860793
Ketanag15 <Ketanag2k@gmail.com>
2025-05-25 14:38:38 +0530

    Contains Duplicates.

---
+++ Practice_Codes/src/DSA/ContainsDuplicate.java
@@ -0,0 +1,26 @@
+package DSA;
+
+import java.util.*;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,1,2,7};
+        int[] arr2 = {1,2,3,4};
+        if(haveDuplicates(arr2)){
+            System.out.println("Contains Duplicates");
+        } else{
+            System.out.println("Doesn't Contains Duplicates");
+        }
+
+    }
+    public static boolean haveDuplicates(int[] arr){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            if(map.containsKey(arr[i])){
+                return  true;
+            }
+            map.put(arr[i], 1);
+        }
+        return false;
+    }
+}
